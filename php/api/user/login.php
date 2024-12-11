f89fb693289eae163ef79dc8678840a776ffcd5c
Marian Figula <majofigulam3@gmail.com>
2024-12-11 17:09:31 +0100

    feat: headers in file

--- php/api/user/login.php
+++ php/api/user/login.php
@@ -24,6 +24,15 @@
  * - Upon successful login, session variables are set to maintain the user's session.
  */
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
+header("Access-Control-Max-Age: 3600"); // Cache the preflight response for 1 hour
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
 header("Content-Type: application/json");
 
 require __DIR__ . '/../../vendor/autoload.php';
