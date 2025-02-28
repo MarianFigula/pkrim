ea451a42f37859fc7528723dc53f902e8cdb85ea
Marian Figula <majofigulam3@gmail.com>
2025-02-28 17:17:55 +0100

    feat: uncommented headers

--- php/api/user/register.php
+++ php/api/user/register.php
@@ -26,10 +26,10 @@
  * Notes:
  * - Upon successful registration, session variables are set to maintain the user's session.
  */
-//header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-//header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
-//header("Access-Control-Max-Age: 3600"); // Cache the preflight response for 1 hour
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
+header("Access-Control-Max-Age: 3600"); // Cache the preflight response for 1 hour
 header("Content-Type: application/json");
 
 require __DIR__ . '/../../vendor/autoload.php';
